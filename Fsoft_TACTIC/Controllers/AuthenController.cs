5d73781873cb18a0274c9ba55a230150ddd21590
Le Tran Hong Duc <duclthde160361@fpt.edu.vn>
2023-03-29 08:57:17 +0700

    Add project files.

---
+++ Fsoft_TACTIC/Controllers/AuthenController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fsoft_TACTIC.Controllers
+{
+    public class AuthenController : Controller
+    {
+        // GET: Web
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fsoft_TACTIC/assets/js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Enter Your Name Below",
+                    minlength: "your name must consist of at least 2 characters"
+                },
+                subject: {
+                    required: "Enter Your Subject",
+                    minlength: "your subject must consist of at least 4 characters"
+                },
+                number: {
+                    required: "Enter Your Number",
+                    minlength: "your Number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "Email Require"
+                },
+                message: {
+                    required: "Please Write Down The Form In Order To Complete The Sequence",
+                    minlength: "Please write down the form more"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
